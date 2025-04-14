e6491fd11cf838df79ec0c309359a84b2aa8a701
zhaote <zhaote@zcckj.com>
2025-04-14 20:32:00 +0800

    feat: setup DrizzleORM

---
+++ src/db/schema.ts
@@ -0,0 +1,20 @@
+import {
+  pgTable,
+  text,
+  uuid,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").unique().notNull(),
+    name: text("name").notNull(),
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
